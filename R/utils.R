e68ed51ec823f81986774ce855da278559d79f13
thomasp85 <thomasp85@gmail.com>
2016-09-29 15:49:04 +0200

    Fix ellipsis

--- R/utils.R
+++ R/utils.R
@@ -14,8 +14,11 @@ is_missing_arg <- function(x) identical(x, quote(expr = ))
 arg_getter <- function() {
     fun <- sys.function(sys.parent(1))
     arg_env <- attr(fun, 'arg_env')
-    vals <- as.list(parent.frame())
-
+    parent <- parent.frame()
+    vals <- as.list(parent)
+    if(!is.null(formals(fun)[['...']])) {
+        vals <- c(vals, eval(quote(list(...)), parent))
+    }
     c(arg_env$args, vals, arg_env$args_end)
 }
 copy_env <- function(env) list2env(as.list(env), parent = parent.env(env))
