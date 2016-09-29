ddb46b837aa36bdcb0e3db204808316f013e1811
thomasp85 <thomasp85@gmail.com>
2016-09-29 15:42:46 +0200

    Improve performance of arg_getter

--- R/utils.R
+++ R/utils.R
@@ -12,22 +12,9 @@
 
 is_missing_arg <- function(x) identical(x, quote(expr = ))
 arg_getter <- function() {
-    env <- parent.frame()
     fun <- sys.function(sys.parent(1))
-    args <- names(formals(fun))
     arg_env <- attr(fun, 'arg_env')
-
-    if (is.null(args)) {
-        vals <- list()
-    } else {
-        vals <- mget(args, envir = env)
-        ellipsis <- names(vals) == '...'
-        if (any(ellipsis)) {
-            vals <- append(vals, eval(quote(list(...)), env), which(ellipsis))
-            vals[ellipsis] <- NULL
-        }
-        vals <- vals[!vapply(vals, is_missing_arg, logical(1))]
-    }
+    vals <- as.list(parent.frame())
 
     c(arg_env$args, vals, arg_env$args_end)
 }
